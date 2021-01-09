cdea3ec55aa4024267775a9bd14d58322c9df300
Yvan Chu <yvchu31@gmail.com>
2021-01-09 11:00:20 +0800

    The iOS app for the AR Monitor

--- CARP 0.0/ViewController.swift
+++ CARP 0.0/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    // Parametres
+    let viewBackgroundColor : UIColor = UIColor.black // UIColor.white
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,20 +26,24 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        // App Setup
+        UIApplication.shared.isIdleTimerDisabled = true
         
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        let configuration = ARImageTrackingConfiguration()
 
+        guard let trackingImages = ARReferenceImage.referenceImages(inGroupNamed: "carp", bundle: nil) else {
+            fatalError("couldn't load tracking images.")
+        }
         // Run the view's session
+        configuration.videoFormat = ARImageTrackingConfiguration.supportedVideoFormats[0]
+        configuration.trackingImages = trackingImages
+        configuration.maximumNumberOfTrackedImages = 4
         sceneView.session.run(configuration)
     }
     
@@ -56,6 +64,38 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         return node
     }
 */
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        
+        // make sure this is an image anchor, otherwise bail out
+        guard let imageAnchor = anchor as? ARImageAnchor else { return nil }
+
+        // create a plane at the exact physical width and height of our reference image
+        let plane = SCNPlane(width: 0.31, height: 0.21)
+
+        let myView = UIImage(named:"GoogleCalendar")
+        
+        if imageAnchor.referenceImage.name == "choco" {
+            // make the plane have a transparent blue color
+            plane.firstMaterial?.diffuse.contents = myView
+        } else {
+            plane.firstMaterial?.diffuse.contents = UIImage(named:"notion")
+        }
+        
+
+        // wrap the plane in a node and rotate it so it's facing us
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.eulerAngles.x = -.pi / 2
+
+        // now wrap that in another node and send it back
+        let node = SCNNode()
+        node.addChildNode(planeNode)
+        return node
+    }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
